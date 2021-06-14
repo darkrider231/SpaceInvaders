8c6806e103dd3d1128827c3e19423687b634b0d4
Grant Patterson <77687606+darkrider231@users.noreply.github.com>
2021-06-14 20:10:02 +0000

    Space Invaders Kaboom.js Game Project

--- scenes/lose.js
+++ scenes/lose.js
@@ -1,6 +1,8 @@
 add([
   text('Final Score: ' + '\n' + args.score),
+  color(0, 0, 1),
   origin('center'),
   scale(7),
-  pos(width() / 2, height() / 2)
+  pos(width() / 2, height() / 2),
+  
 ])
---
+++ scenes/win.js
@@ -0,0 +1,7 @@
+add([
+  text('YOU WIN' + '\n' + 'Final Score: ' + '\n' + args.score),
+  color(0, 0, 1),
+  origin('center'),
+  scale(6),
+  pos(width()/2, height()/2)
+])
