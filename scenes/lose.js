35d3b304f7fc7c0f23699c252d977952c7290970
Grant Patterson <77687606+darkrider231@users.noreply.github.com>
2021-06-15 21:55:03 +0000

    Space Invaders Kaboom.js Game Project

--- scenes/lose.js
+++ scenes/lose.js
@@ -1,4 +1,12 @@
 add([
+  text('GAME OVER!'),
+  color(1, 0, 0),
+  origin('center'),
+  scale(7),
+  pos(width() / 2, height() / 3),
+])
+
+add([
   text('Final Score: ' + '\n' + args.score),
   color(0, 0, 1),
   origin('center'),
